db7369df43ecea0fdc9ec58639b04ccb75251d11
robertspg <rs@imac-r.home>
2022-11-11 11:33:51 +0100

    updated main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -1,16 +1,38 @@
-#include <iostream>
-#include <vector>
-#include <string>
+#include "SFML/Graphics.hpp"
 
-using namespace std;
+int main() {
+	sf::RenderWindow renderWindow(sf::VideoMode(640, 480), "Dragon game");
 
-int main()
-{
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+	sf::Event event;
+	sf::Texture tekstura;
+	tekstura.loadFromFile("smok.png");//laduj teksture
 
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+	sf::IntRect ksztaltSmoka(0, 0, 200, 200);//okresl obszar spritea
+	sf::Sprite smok(tekstura, ksztaltSmoka);//utworz spritea powiazanego z tekstura
+	sf::Clock zegar;//zegarek do odmierzania czasu klatki
+	//ksztaltSmoka.top = 200;//ustaw sie na kolejny wiersz tekstury
+	while (renderWindow.isOpen()) {
+		while (renderWindow.pollEvent(event)) {
+			if (event.type == sf::Event::EventType::Closed)
+				renderWindow.close();
+		}
+		//glowna procedura animacji, klatka trwa 300ms
+		if (zegar.getElapsedTime().asMilliseconds() > 30.0f) {
+			if (ksztaltSmoka.left == 400)//w wierszu tekstury sa 3 klatki (200 x 200px)
+				ksztaltSmoka.left = 0;
+			else
+				ksztaltSmoka.left += 200;//przemieszczaj sie w poziomie tekstury
+			smok.move(sf::Vector2f(10.0f, 10.0f));//przemieszczaj smoka
+			if (smok.getPosition().y > 480)
+				smok.setPosition(sf::Vector2f(10.0f, 10.0f));
+
+			smok.setTextureRect(ksztaltSmoka);
+			zegar.restart();
+		}
+
+
+		renderWindow.clear();
+		renderWindow.draw(smok);
+		renderWindow.display();
+	}
 }
